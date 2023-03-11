c4949f99bf9b910dabfd67708b6dd743d0ea93d5
herereadthis <herereadthis.github@gmail.com>
2023-03-10 23:17:27 -0500

    more transfer

--- src/utils/dump1090_utils.py
+++ src/utils/dump1090_utils.py
@@ -15,23 +15,8 @@ def get_receiver_options(base_url):
 
 
 def get_aircraft(base_url, latitude, longitude):
-    receiver_url = f'{base_url}/data/aircraft.json'
-
-    response = requests.get(receiver_url)
-    json_obj = json.loads(response.content)
-    print('retrieved aircraft response.')
-
-    filtered_aircraft = [
-        d for d in json_obj['aircraft']
-        if "lat" in d and "lon" in d and d["lat"] is not None and d["lon"] is not None
-    ]
-
-    mapped_keys = ['lat', 'lon', 'alt_geom', 'hex', "flight", "category", "gs", "true_heading"]
-    mapped_aircraft = [{k: v for k, v in d.items() if k in mapped_keys} for d in filtered_aircraft]
-
-    print(f'latitude: {latitude}')    
-    print(f'longitude: {longitude}')    
-    mapped_aircraft = aircraft.add_aircraft_options(mapped_aircraft, latitude, longitude)
+    aircraft_messages = aircraft.get_aircraft(base_url, True)  
+    mapped_aircraft = aircraft.add_aircraft_options(aircraft_messages, latitude, longitude)
     pprint(mapped_aircraft[0])
 
     return mapped_aircraft
