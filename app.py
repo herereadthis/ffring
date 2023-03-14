3965d9d0e6c82a1cbf5e2d63e141aba961f08f83
herereadthis <herereadthis.github@gmail.com>
2023-03-14 01:02:11 -0400

    hexdb

--- app.py
+++ app.py
@@ -54,14 +54,10 @@ def get_index():
     base_lat = receiver_options["lat"]
     base_lon = receiver_options["lon"]
 
-    timezonefinder_s = time.time()
     if (session.get('local_timezone_name') is None):
         local_timezone = adsb_tools.timezone.get_timezone_name(base_lat, base_lon)
         session['local_timezone_name'] = local_timezone
 
-    timezonefinder_e = time.time()
-
-    weather_s = time.time()
     if (session.get('forcast_hourly_url') is None or session.get('weather_timezone') is None):
         forecast_hourly_url, weather_timezone_name = weather_utils.get_grid_data(base_lat, base_lon)
         session['forcast_hourly_url'] = forecast_hourly_url
@@ -71,32 +67,39 @@ def get_index():
         # There should be only one source of truth for timezone name!
         session.get('forcast_hourly_url'), session.get('weather_timezone_name')
     )
-    weather_e = time.time()
-    aircraft_s = time.time()
     aircraft_list = dump1090_utils.get_aircraft(base_adsb_url, base_lat, base_lon)
-    photo_s = time.time()
-    aircraft_e = time.time()
-    aircraft_hex = aircraft_list[0]['hex']
-    aircraft_hex = 'aa2b21' 
-    print(aircraft_hex)
+
+    nearest_aircraft = aircraft_list[0]
+    icao_24 = nearest_aircraft['icao']
+    nearest_aircraft['hexdb'] = {
+        'aircraft_url': f'https://hexdb.io/api/v1/aircraft/{icao_24}',
+        'conversion_url': f'https://hexdb.io/hex-reg?hex={icao_24}'
+    }
+    print(f"nearest_aircraft_icao is {icao_24}")
+
     aircraft_image = {}
-    if (aircraft_hex is not None):
-        aircraft_image = adsb_tools.aircraft.get_aircraft_image(aircraft_hex)
-    photo_e = time.time()
 
+    if (not session.get('nearest_aircraft.icao') or session.get('nearest_aircraft.icao') != icao_24):
+        print('nearest aircraft not previously detected')
+        session['nearest_aircraft'] = nearest_aircraft
+        if (icao_24):
+            aircraft_image = adsb_tools.aircraft.get_aircraft_image(icao_24)
+            session['nearest_aircraft']['image'] = aircraft_image
+    else:
+        print('still following the same nearest aircraft')
+        aircraft_image = session.get('nearest_aircraft.image', {})
 
+    pprint(session)
 
-
-    print(f'elapsed time timezone: {timezonefinder_e - timezonefinder_s}')
-    print(f'elapsed time weather: {weather_e - weather_s}')
-    print(f'elapsed time aircraft: {aircraft_e - aircraft_s}')
-    print(f'elapsed time photo: {photo_s - photo_e}')
 
     stuff['weather_report'] = weather_report
     stuff['receiver_options'] = receiver_options
     stuff['aircraft_list'] = aircraft_list
     stuff['local_timezone_name'] = session.get('local_timezone_name')
     stuff['aircraft_image'] = aircraft_image
+    stuff['nearest_aircraft'] = session.get('nearest_aircraft')
+
+    pprint(stuff['nearest_aircraft'])
 
     return render_template('index.html', **stuff)
 
