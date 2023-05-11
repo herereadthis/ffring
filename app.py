bd9c8dc7d1771b3876b0068a55c4cfc344c844f3
herereadthis <herereadthis.github@gmail.com>
2023-05-10 23:31:31 -0400

    direction

--- app.py
+++ app.py
@@ -136,25 +136,22 @@ def draw_line(angle, unit_size):
     center_x, center_y = 0,0
 
     # Calculate the endpoint of the line based on the given angle
-    end_x = center_x + math.cos(math.radians(angle))
-    end_y = center_y - math.sin(math.radians(angle))
+    end_x = math.sin(math.radians(angle))
+    end_y = 0 - math.cos(math.radians(angle))
 
     # Scale the coordinates by the unit size
-    center_x *= unit_size
-    center_y *= unit_size
-    end_x *= unit_size
-    end_y *= unit_size
+    center_x += unit_size
+    center_y += unit_size
+    end_x = end_x * unit_size + unit_size
+    end_y = end_y * unit_size + unit_size
 
     # Build the SVG code as a string
     svg_code = '<svg viewBox="0 0 ' + str(2*unit_size) + ' ' +  str(2*unit_size) +'" xmlns="http://www.w3.org/2000/svg">'
-    # svg_code += '<rect x="0" y="0" width="' + str(4*unit_size) + '" height="' + str(4*unit_size) + '" fill="none" stroke="black" stroke-width="2" />'
     svg_code += '<line x1="' + str(center_x) + '" y1="' + str(center_y) + '" x2="' + str(end_x) + '" y2="' + str(end_y) + '" stroke="red" stroke-width="2" />'
     svg_code += '</svg>'
 
     # Return the SVG code as a string
     return svg_code
-
-
 
 env = Environment(loader=FileSystemLoader('templates'))
 env.filters['format_datetime'] = dt_utils.format_datetime
